e31df84e0e1f2963a9de33b27bad12a12d579e5b
James Freiwirth <james@ciseware.com>
2012-07-07 22:24:39 +0100

    Set up debugging and JRebel

--- sbt.bat
+++ sbt.bat
@@ -1,2 +1,3 @@
+set JREBELJAR=C:\Program Files (x86)\ZeroTurnaround\JRebel\jrebel.jar
 set SCRIPT_DIR=%~dp0
-java -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m -Xmx712M -Xss2M -jar "%SCRIPT_DIR%\sbt-launcher.jar" %*
+java -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=512m -Xmx712M -Xss2M -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -javaagent:"%JREBELJAR%" -jar "%SCRIPT_DIR%\sbt-launcher.jar" %*
